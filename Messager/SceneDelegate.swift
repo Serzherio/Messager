43e1457ce401ed7a931f2f162d9a2c30f95edf58
Federico <Serzherio@yandex.ru>
2022-01-02 13:46:15 +0300

    lol
    test

--- Messager/SceneDelegate.swift
+++ Messager/SceneDelegate.swift
@@ -19,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = AuthViewController()
+//        window?.rootViewController = AuthViewController()
+        window?.rootViewController = LoginViewController()
 //        if let user = Auth.auth().currentUser {
 //            FirestoreService.shared.getUserData(user: user) { result in
 //                switch result {
@@ -30,6 +31,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //                case .failure(let error):
 //                    self.window?.rootViewController = AuthViewController()
 //                }
+        
+        
 //            }
 //        } else {
 //            window?.rootViewController = AuthViewController()
